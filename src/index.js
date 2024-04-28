ebc356fe759b6599dd8fb7b8596bcd47cc981043
Chandru C P <Chandrucld6@gmail.com>
2024-04-28 15:18:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+  "https://uskzrnzxrgipnthybiwc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVza3pybnp4cmdpcG50aHliaXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQyOTIyMjcsImV4cCI6MjAyOTg2ODIyN30.MuuLI9LG4DhBiLwBiySg7fICPWs6_uqsjhN1raEQUpg"
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
